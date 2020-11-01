88a071bd7485644e73c4fb4bf2ce766c9870625e
sunpyopark <photo10004@gmail.com>
2020-11-01 11:57:14 +0900

    change ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "web" {
   ami = "ami-021809d9177640a20" # amzn2-ami-hvm-2.0.20200207.1-x86_64-gp2
   instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform EC2"
+  }
 }
